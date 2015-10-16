a3de19ba8a448b796a8de4779b33aba1eee1f5f7
Alan Shaw <alan@tableflip.io>
2015-10-16 11:17:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# boom-browserify [![Build Status](https://secure.travis-ci.org/alanshaw/boom-browserify.png)](http://travis-ci.org/alanshaw/boom-browserify)
+[Boom](https://github.com/hapijs/boom) for the browser.
+
+This module provides a browserify compatible version of Boom that you should be able to use in most browsers. It aims to not use non-standard JavaScript features and have a small bundle size.
+
+Please see the [Boom docs](https://github.com/hapijs/boom/blob/master/README.md) for usage and examples.
---
+++ shim/hoek.js
@@ -0,0 +1,32 @@
+exports.assert = function (condition /*, msg1, msg2, msg3 */) {
+
+    if (condition) {
+        return;
+    }
+
+    if (arguments.length === 2 && arguments[1] instanceof Error) {
+        throw arguments[1];
+    }
+
+    var msgs = [];
+    for (var i = 1, il = arguments.length; i < il; ++i) {
+        if (arguments[i] !== '') {
+            msgs.push(arguments[i]);            // Avoids Array.slice arguments leak, allowing for V8 optimizations
+        }
+    }
+
+    msgs = msgs.map(function (msg) {
+
+        return typeof msg === 'string' ? msg : msg instanceof Error ? msg.message : exports.stringify(msg);
+    });
+    throw new Error(msgs.join(' ') || 'Unknown error');
+};
+
+exports.escapeHeaderAttribute = function (attribute) {
+
+    // Allowed value characters: !#$%&'()*+,-./:;<=>?@[]^_`{|}~ and space, a-z, A-Z, 0-9, \, "
+
+    exports.assert(/^[ \w\!#\$%&'\(\)\*\+,\-\.\/\:;<\=>\?@\[\]\^`\{\|\}~\"\\]*$/.test(attribute), 'Bad attribute value (' + attribute + ')');
+
+    return attribute.replace(/\\/g, '\\\\').replace(/\"/g, '\\"');                             // Escape quotes and slash
+};
